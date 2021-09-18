3ea80b55af735540e40f4a99d9910f5a575b36e4
Vishnunarayan K I <appukuttancr@gmail.com>
2021-09-19 02:38:23 +0530

    linux: add support for pie binaries

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::process::Command;
 use std::collections::HashMap;
+use std::path::Path;
+use std::process::Command;
 
 use defs::ProcessInfo;
+use object::Object;
 use ptrace_engine::Process;
-use tracing_subscriber;
 use tracing::{debug, warn};
+use tracing_subscriber;
 
 mod breakpoint;
 mod defs;
@@ -13,29 +15,46 @@ mod obj_helper;
 mod ptrace_engine;
 mod utils;
 
-use crate::defs::{Pid, Result};
 use crate::defs::{DebuggerEngine, DebuggerStatus};
+use crate::defs::{Pid, Result};
 use crate::obj_helper::get_functions;
+use crate::utils::get_base_region;
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
-    let cmd = Command::new("./fact");
+
+    let binary = Path::new("./fact-pie");
+
+    let cmd = Command::new(binary);
     let (mut engine, child) = ptrace_engine::PtraceEngine::spawn(cmd)?;
 
-    let bin_data = std::fs::read("./fact")?;
+    let bin_data = std::fs::read(binary)?;
     let obj_file = object::File::parse(&*bin_data)?;
+    let binary_is_relocatable = matches!(
+        obj_file.kind(),
+        object::ObjectKind::Dynamic | object::ObjectKind::Relocatable
+    );
     let funcs = get_functions(&obj_file);
     // debug!(?funcs);
+
     let mut global_pid = Pid::from_raw(child.id() as i32);
     let mut funcs_map = HashMap::new();
+
     let process = &mut Process(global_pid);
     let maps = process.get_memory_maps()?;
     debug!(?maps);
+    let base_region = get_base_region(&maps, binary.canonicalize()?.to_str().unwrap()).unwrap();
+    debug!(?binary_is_relocatable, ?base_region);
 
     for func in funcs.into_iter() {
-        debug!("breakpoint set at {}", func.address);
-        engine.set_breakpoint(global_pid, func.address)?;
-        funcs_map.insert(func.address, func);
+        let bp_addr = if binary_is_relocatable {
+            base_region.start + func.address
+        } else {
+            func.address
+        };
+        debug!("breakpoint set at {}", bp_addr);
+        engine.set_breakpoint(global_pid, bp_addr)?;
+        funcs_map.insert(bp_addr, func);
     }
     engine.cont(global_pid)?;
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,20 @@
 use std::num::ParseIntError;
 
+use crate::defs::MemoryRegion;
+
 // Parses hex address starting with 0x
 pub fn parse_address(s: &str) -> Result<u64, ParseIntError> {
     let s = s.trim_start_matches("0x");
     u64::from_str_radix(s, 16)
 }
 
-
 pub fn parse_address_without_0x(s: &str) -> Result<u64, ParseIntError> {
     u64::from_str_radix(s, 16)
+}
+
+pub fn get_base_region<'a>(vmmap: &'a [MemoryRegion], filename: &str) -> Option<&'a MemoryRegion> {
+    vmmap
+        .into_iter()
+        .filter(|region| matches!(region.filename, Some(ref file) if file == filename))
+        .min_by_key(|region| region.start)
 }
